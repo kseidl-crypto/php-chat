cbcfe4bd4834def63f6b047c69562407ebf32928
kseidl <kseidl@student.tgm.ac.at>
2021-03-29 14:46:41 +0200

    Seiten erstellt

---
+++ PHPTemplate/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+=
+php/index.php,2\2\2280de4f18d01c53cf6fd5543744fd4e661aaa47
---
+++ PHPTemplate/php/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>PHP Template Website</title>
+    <link rel="stylesheet" href="../css/bootstrap.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1>PHP Template Website</h1>
+</body>
+</html>
