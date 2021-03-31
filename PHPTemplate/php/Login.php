d8558d475dd487b986a7229c82407ba013dc5bb9
kseidl <kseidl@student.tgm.ac.at>
2021-03-31 14:35:04 +0200

    Registrierung und Login grundbau !!Zu tun eingefügt

--- PHPTemplate/php/Login.php
+++ PHPTemplate/php/Login.php
@@ -1,6 +1,29 @@
 <?php
+
+//Zu tun: Session
+//        passwort entschluesseln
+//        zu Chat weiterleiten
+//
+
+
 session_start();
 
+//überprüfung durch benutzernamen und passwort
 
+$benutzer = $_POST('benutzer');
+$pw = $_POST('passwort');
+
+//laden alle Zeilen von text.txt
+//!!!Dateipfad überprüfen!!!
+$user = file("../Dateien/Text.txt");
+
+//foreach um alle Einträge durchzugehen
+foreach($user AS $line){
+    //Teilen bei Semikolon bei Line
+    $userInfo = explode(";", $line);
+    if($userInfo[0] == $benutzer AND $userInfo[1] == $pw){
+        //weiterleiten
+    }
+}
 
 ?>
--- PHPTemplate/php/Registrierung.php
+++ PHPTemplate/php/Registrierung.php
@@ -1,4 +1,10 @@
 <?php
+//Zu tun: Passwort verschluesseln!
+//        überprüfen
+//        weiterleiten auf loggin seite
+//        Captcha
+
+
 
 //Wert von index-seite geholt und in Variablen gespeichert
 $vorname = $_POST['vorname'];
@@ -6,10 +12,19 @@ $nachname = $_POST['nachname'];
 $email = $_POST['email'];
 $benutzer = $_POST['benutzername'];
 $passwd = $_POST['passwort'];
+$user = array($vorname, $nachname, $email, $benutzer, $passwd, $passwd);
+
+//checkt, ob variablen leer sind
+if( ! empty($vorname)  AND ! empty($nachname) AND ! empty($email) AND ! empty($benutzer) AND ! empty($passwd)){
+    //semikolon als Trennzeichen, Variablen verbinden
+    $eintrag  = implode (";", $user)."\r\n";
+    //!!!!!Schaun, ob es überschrieben wird!!! Und ob Dateipfad stimmt!!!
+    //Ende hinzugefügt
+    file_put_contents("../Dateien/Text.txt", $eintrag, FILE_APPEND);
+    echo "Registrierung abgeschlossen";
+}else{
+    echo "Bitte alle Felder ausfüllen";
+}
 
 
-
-$user = new Benutzer();
-$user ->nachname="Herbert";
-echo "". $user -> getNachname() ;
 ?>
