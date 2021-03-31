bd1376ca4ae7244cc7b127e21a8bc56b794f7bea
kseidl <kseidl@student.tgm.ac.at>
2021-03-31 14:55:19 +0200

    Chat

--- PHPTemplate/php/Chat.php
+++ PHPTemplate/php/Chat.php
@@ -1,6 +1,29 @@
 <?php
-//Zu tun: mit JS aktualisierung des CLientens
-//
+//Zu tun: mit JS aktualisierung des Clientens
+
+date_default_timezone_set("Europe/Vienna");
+
+$benutzer = $_POST('benutzer');
+$time = time();
+$datum = date("d.m.Y",$time);
+$uhrzeit = date("H:i",$time);
+$text = $_POST('text');
+
+//in einen String alle Werte gespeichert
+//voherigen Werte dazugehängt
+$text = file_get_contents('../Dateien/Nachrichten.txt')."$datum\n$benutzer, $time - \"$text\"\n";
+
+
+$handle = fopen('test.txt',"r+" );
+//in File text geschrieben
+fwrite($handle, $text);
+
+
+fclose($handle);
+
+
+//Text ausgegeben
+echo file_get_contents('test.txt');
 
 
 ?>
