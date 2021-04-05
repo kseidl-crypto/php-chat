1e1600af25cae42724aa13a08006189f2f9ca63a
Mona Eisenkirchner <meisenkirchner@student.tgm.ac.at>
2021-04-06 00:08:56 +0200

    LoginHTML.php Bootstrap implementiert

--- PHPTemplate/php/LoginHTML.php
+++ PHPTemplate/php/LoginHTML.php
@@ -1,10 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 </head>
-<body>
 
+<body>
+<div class="row">
+    <div class="col-sm-1">
+        <h1>Login</h1>
+    </div>
+</div>
+<form action="./Login.php" method="POST">
+    <div class="form-group">
+        <label class="control-label col-sm-2" for="benutzer">Benutzername:</label>
+        <div class="col-sm-10">
+            <input type="text" class="form-control" id="benutzer" placeholder="Benutzername">
+        </div>
+    </div>
+    <div class="form-group">
+        <label class="control-label col-sm-2" for="passwort">Passwort:</label>
+        <div class="col-sm-10">
+            <input type="email" class="form-control" id="passwort" placeholder="Passwort">
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-default">Submit</button>
+        </div>
+    </div>
+</form>
 </body>
 </html>
