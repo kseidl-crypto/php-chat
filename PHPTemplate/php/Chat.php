7d81089369b9a516bb3662b7c4ab5032e186cd98
Mona Eisenkirchner <meisenkirchner@student.tgm.ac.at>
2021-05-04 16:10:42 +0200

    Nachrichten JSON

--- PHPTemplate/php/Chat.php
+++ PHPTemplate/php/Chat.php
@@ -1,23 +1,37 @@
 <?php
+namespace chat;
+session_start();
 //Zu tun: mit JS aktualisierung des Clientens
 //        Benutzer von der anderen Seite holen
 
 date_default_timezone_set("Europe/Vienna");
 
 $benutzer = $_SESSION['benutzer'];
-echo $benutzer;
 $time = time();
 $datum = date("d.m.Y",$time);
 $uhrzeit = date("H:i",$time);
 $text = $_POST['text'];
 
+$dat = file_get_contents("./Dateien/Nachrichten.json");
+$dat = str_replace(']',',',$dat);
+$dat = $dat."{\"user\":\"$benutzer\",\"date\":\"$datum\",\"text\":\"$text\"}]";
+
+file_put_contents("./Dateien/Nachrichten.json",$dat);
+
+
 //in einen String alle Werte gespeichert
 //voherigen Werte dazugehängt
-$text = file_get_contents('./Dateien/Nachrichten.txt')."$datum\n$benutzer, $time - \"$text\"\n";
+//$text = file_get_contents('./Dateien/Nachrichten.json')."$datum\n$benutzer, $time - \"$text\"\n";
+
+$json_arr[] = array(
+    "user" => $benutzer,
+    "date" => $datum,
+    "text" => $text
+);
+//file_put_contents("./Dateien/Nachrichten.json", json_encode($json_arr), FILE_APPEND);
 
 
-file_put_contents('./Dateien/Nachrichten.txt', "$datum\n$benutzer, $time - \"$text\"\n",
-    FILE_APPEND);
+//file_put_contents('./Dateien/Nachrichten.json', "{\"benutzer\":\"$benutzer\"}",FILE_APPEND);
 
 
 //Text ausgegeben
@@ -25,12 +39,12 @@ file_put_contents('./Dateien/Nachrichten.txt', "$datum\n$benutzer, $time - \"$te
 //    ob_end_clean();
  //   echo file_get_contents('./Dateien/Nachrichten.txt');
 //}
-
+/*
 ob_start();
 ob_flush();
 ob_end_clean();
 flush();
-echo file_get_contents('./Dateien/Nachrichten.txt');
+echo file_get_contents('./Dateien/Nachrichten.json');
 
 ob_end_flush();
-?>
+*/
