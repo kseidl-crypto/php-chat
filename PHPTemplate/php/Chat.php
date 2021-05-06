88bd4adf6214a811a8a9c7917700a23380bfc52b
Mona Eisenkirchner <meisenkirchner@student.tgm.ac.at>
2021-05-06 11:15:56 +0200

    AAAAAAAAAAAAAAAAAAAAA

--- PHPTemplate/php/Chat.php
+++ PHPTemplate/php/Chat.php
@@ -12,26 +12,26 @@ $datum = date("d.m.Y",$time);
 $uhrzeit = date("H:i",$time);
 $text = $_POST['text'];
 
-$dat = file_get_contents("./Dateien/Nachrichten.json");
-$dat = str_replace(']',',',$dat);
-$dat = $dat."{\"user\":\"$benutzer\",\"date\":\"$datum\",\"text\":\"$text\"}]";
+//$dat = str_replace(']',',',$dat);
+//$dat = $dat."{\"user\":\"$benutzer\",\"date\":\"$datum\",\"text\":\"$text\"}]";
 
-file_put_contents("./Dateien/Nachrichten.json",$dat);
-
+file_put_contents("./Dateien/Nachrichten.txt","$benutzer||$datum||$text\n", FILE_APPEND);
+$dat = explode("\n", file_get_contents("./Dateien/Nachrichten.txt"));
+echo implode("<br>",$dat);
 
 //in einen String alle Werte gespeichert
 //voherigen Werte dazugehängt
-//$text = file_get_contents('./Dateien/Nachrichten.json')."$datum\n$benutzer, $time - \"$text\"\n";
+//$text = file_get_contents('./Dateien/Nachrichten.txt')."$datum\n$benutzer, $time - \"$text\"\n";
 
 $json_arr[] = array(
     "user" => $benutzer,
     "date" => $datum,
     "text" => $text
 );
-//file_put_contents("./Dateien/Nachrichten.json", json_encode($json_arr), FILE_APPEND);
+//file_put_contents("./Dateien/Nachrichten.txt", json_encode($json_arr), FILE_APPEND);
 
 
-//file_put_contents('./Dateien/Nachrichten.json', "{\"benutzer\":\"$benutzer\"}",FILE_APPEND);
+//file_put_contents('./Dateien/Nachrichten.txt', "{\"benutzer\":\"$benutzer\"}",FILE_APPEND);
 
 
 //Text ausgegeben
@@ -44,7 +44,16 @@ ob_start();
 ob_flush();
 ob_end_clean();
 flush();
-echo file_get_contents('./Dateien/Nachrichten.json');
+echo file_get_contents('./Dateien/Nachrichten.txt');
 
 ob_end_flush();
 */
+?>
+<!DOCTYPE html>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="./js/JSChat.js"></script>
+</head>
+<body>
+</body>
+</html>
